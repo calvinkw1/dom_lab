2610ed731502e17abf71b9b2f3266043161315cb
Calvin Wang <calvinkw1@gmail.com>
2015-01-15 19:59:42 -0800

    finished DOM homework

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 // Your code here
+
+
+function changeGreet(greeting) {
+	greeting = "Hello, Planet Earth!";
+}
+
+
+function listToYellow(listItems) {
+	for (var i = 0; i < listItems.length; i++) {
+		listItems[i].style.backgroundColor = "yellow";
+	    listItems[i].addEventListener('click', addClass);
+	}
+}
+
+function addClass(event) {
+	this.className = "selected";
+	document.querySelector("img").setAttribute("src", "./images/" + this.innerHTML + ".jpeg");
+}
+
+
+var resetAll = function() {
+	list_items = document.querySelectorAll("li");  
+	for (var i = 0; i < list_items.length; i++) {
+		list_items[i].className = ""; 
+	}
+	document.querySelector("img").setAttribute("src", "./images/panic.jpeg");
+};
+
+
+
+
+
+
+
+window.addEventListener("load", function() {
+	var greetMsg = document.getElementById("greeting").innerHTML;
+	var list = document.getElementsByTagName("li");
+	var grabAllItems = document.querySelectorAll("body");
+
+	changeGreet(greetMsg);
+	listToYellow(list);
+	document.getElementById("reset").addEventListener("click", resetAll);
+
+
+
+
+
+
+
+});
